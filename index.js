bca18d6c9af4642a785ae2471417271b2c3ba7fc
abraham <apolichenco@gmail.com>
2022-06-08 14:52:43 -0400

    commit tree 4f9bbbc2a185c26102d5db04939fe36b273f3abb

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+   if (number >= 42) {
+    return number - 42;
+}   else return 42 - number;
+   } 
+function distanceFromHqInFeet(number) {
+   return distanceFromHqInBlocks(number) * 264;
+}
+function distanceTravelledInFeet(number, number2) {
+    if (number >= number2){
+        return (number - number2) * 264;     
+         }
+         else {return (number2 - number) * 264;}
+}
+function calculatesFarePrice(start, destination) {
+
+    if (start >= destination) {
+       const number = (start - destination) * 264;
+        if (number < 400) {
+        return 0;}
+    else if (number > 2500) {
+        return "cannot travel that far";}
+   else if (number > 2000) {
+        return 25;}
+   else if (number < 2000) {
+        return (number - 400) * .02;}
+   }
+
+   if (start < destination) {
+    const number = (destination - start) * 264;
+     if (number < 400) {
+     return 0;}
+ else if (number > 2500) {
+     return "cannot travel that far";}
+else if (number > 2000) {
+     return 25;}
+else if (number < 2000) {
+     return (number - 400) * .02;}
+}}
